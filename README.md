2b43e1c9f08c66e1d0c6e8c419f0fdd12896b682
PhoB <hsp003636@gmail.com>
2024-04-11 20:45:44 -0400

    modify sh file

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 txt파일을 읽어서  
 해당 파일의 내용을 기반으로 rules파일 생성  
 
+```sh make.sh [target_site.txt]```
 
-
-![alt text](image-1.png)
+![image](image.png)
--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 # 파일명
 RULES_FILE="filter.rules"
-FILE_PATH="target_site.txt"
+FILE_PATH=$1
 
 # Suricata 규칙 시작 번호
 SID=10001
@@ -13,13 +13,9 @@ create_rule() {
   local sid_http=$2
   local sid_https=$3
 
-  # 이미 존재하는 서명인지 확인
-  if grep -q "msg:\"Site $site Access\"" $RULES_FILE; then
-    echo "이미 존재하는 서명입니다: Site $site Access"
-  else
-    echo "alert tcp any any -> any 80 (msg:\"Site $site Access\"; content:\"Host: $site\"; http_uri; nocase; sid:$sid_http; rev:1;)" >> $RULES_FILE
-    echo "alert tcp any any -> any 443 (msg:\"Site $site HTTPS Access\"; tls_sni; content:\"$site\"; nocase; sid:$sid_https; rev:1;)\n" >> $RULES_FILE
-  fi
+  echo "alert tcp any any -> any 80 (msg:\"Site $site Access\"; content:\"Host: $site\"; http_uri; nocase; sid:$sid_http; rev:1;)" >> $RULES_FILE
+  echo "alert tcp any any -> any 443 (msg:\"Site $site HTTPS Access\"; tls_sni; content:\"$site\"; nocase; sid:$sid_https; rev:1;)\n" >> $RULES_FILE
+  
 }
 
 # 룰 파일 존재하면 삭제
